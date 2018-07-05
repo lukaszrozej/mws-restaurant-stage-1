117a6455092857023f066a20d9712fed9e296cf5
Łukasz Rożej <lukasz.rozej@gmail.com>
2018-07-05 10:47:47 +0200

    Add service worker registration

--- js/register-sw.js
+++ js/register-sw.js
@@ -1,2 +1,7 @@
-if (!navigator.serviceWorker) return;
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register('/js/sw.js')
+    .then(() => { console.log('Service worker registered.') })
+    .catch(() => { console.log('Service worker registration failed.') })
+}
 
